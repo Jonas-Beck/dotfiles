6867bb5f04b0c2b52d2d74b9dc84b7944b0210dd
Jonas Beck <jonasb@outlook.dk>
2024-10-19 16:16:03 +0200

    feat(neovim): add blink-cmp

---
+++ nvim/.config/nvim/lua/jonasbeck/plugins/lsp/blink-cmp.lua
@@ -0,0 +1,47 @@
+return {
+	"saghen/blink.cmp",
+	lazy = false, -- lazy loading handled internally
+	-- optional: provides snippets for the snippet source
+	dependencies = "rafamadriz/friendly-snippets",
+
+	-- use a release tag to download pre-built binaries
+	version = "v0.*",
+	-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	-- build = 'cargo build --release',
+	-- If you use nix, you can build from source using latest nightly rust with:
+	-- build = 'nix run .#build-plugin',
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		trigger = {
+			completion = {
+				keyword_range = "full", -- full|prefix
+			},
+		},
+		keymap = {
+			show = "<D-c>",
+			hide = "<S-CR>",
+			accept = "<CR>",
+			select_next = { "<Tab>", "<Down>" },
+			select_prev = { "<S-Tab>", "<Up>" },
+			scroll_documentation_down = "<PageDown>",
+			scroll_documentation_up = "<PageUp>",
+		},
+		highlight = {
+			-- sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- useful for when your theme doesn't support blink.cmp
+			-- will be removed in a future release, assuming themes add support
+			use_nvim_cmp_as_default = true,
+		},
+		-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+		-- adjusts spacing to ensure icons are aligned
+		nerd_font_variant = "normal",
+
+		-- experimental auto-brackets support
+		-- accept = { auto_brackets = { enabled = true } }
+
+		-- experimental signature help support
+		-- trigger = { signature_help = { enabled = true } }
+	},
+}
